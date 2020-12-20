4162088ce4515880e3fc4e8ffc94ba2923a73330
Johann Taylor <j.taylor1343@gmail.com>
2020-12-20 02:13:35 -0500

    Added conditions for character choices

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// Array of special characters to be included in password
+// Arrays for possible character types to be used in generated password
 var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-
-// Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 //Choose password length
@@ -31,48 +21,48 @@ function generatePassword() {
   }
   
   //Variables to store selected character types
+  var hasUpperCase = confirm("Click OK to include upper case characters");
+  var hasLowerCase = confirm("Click OK to include lower case characters");
+  var hasNumeric = confirm("Click OK to include numbers");
+  var hasSpecial = confirm("Click OK to include special characters");
 
-  var upper = confirm("Click OK to include upper case characters");
-  var lower = confirm("Click OK to include lower case characters");
-  var numbers = confirm("Click OK to include numbers");
-  var special = confirm("Click OK to include special characters");
-
-  //Store choices for selected character types
+  //Loop to check if at least one variable is chosen
   while (
-    upper === false &&
-    lower === false &&
-    numbers === false &&
-    special === false
-    ){
+    hasUpperCase === false && hasLowerCase === false && hasNumeric === false && hasSpecial === false) {
       alert("You need to choose at least ONE character type");
-      upper = confirm("Click OK to include upper case characters");
-      lower = confirm("Click OK to include lower case characters");
-      numbers = confirm("Click OK to include numbers");
-      special = confirm("Click OK to include special characters");
+      hasUpperCase = confirm("Click OK to include upper case characters");
+      hasLowerCase = confirm("Click OK to include lower case characters");
+      hasNumeric = confirm("Click OK to include numbers");
+      hasSpecial = confirm("Click OK to include special characters");
     }
+    var result = [];
 
-    var charType = {
-      length:length, upper: upper, lower: lower, numbers: numbers, special: special
-    };
+    if (hasUpperCase==true) {
+      result.push(upperCasedCharacters);
+    }
+    if (hasLowerCase==true) {
+      result.push(lowerCasedCharacters);
+    }
+    if (hasNumeric==true) {
+      result.push(numericCharacters);
+    }
+    if (hasSpecial==true) {
+      result.push(specialCharacters);
+    }
+    
+  }
 
-    return charType;
-}
-
-
-
-
-  
- 
 
 
 
 
 
-//Generate password after selecting criteria
-
+var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
   var password = generatePassword();
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -81,5 +71,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
